8e4e86329d35198c26179d9657b01b55dd6dbff0
Ad450 <adjeimanue@gmail.com>
2022-07-19 14:41:59 +0000

    initial react-ts tailwind css setup

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <header>       
+        <h3 className = "font-sm underline decoration-gray-800 text-red-700"> This is the test text</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
