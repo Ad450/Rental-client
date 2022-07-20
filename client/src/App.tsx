170e3a1dff70ad7e79f26b665bd5f8f375ae5f61
Ad450 <adjeimanue@gmail.com>
2022-07-20 14:10:43 +0000

    minor changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,17 @@
-import React from 'react';
+
 
 function App() {
+    
   return (
     <div>
       <header>       
         <h3 className = "font-sm underline decoration-gray-800 text-red-700"> This is the test text</h3>
+
+        <h4>Why no test showing </h4>
       </header>
     </div>
   );
 }
 
 export default App;
+
