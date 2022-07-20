aa181d3b46c93b4e9a00586437a46dab38efc34e
Ad450 <adjeimanue@gmail.com>
2022-07-20 23:24:24 +0000

    auth repo implemented

---
+++ client/src/features/authentication/repository/auth_repo_impl.ts
@@ -0,0 +1,31 @@
+import { LoginParam, SignupParam } from "../../../core/types.ts/usecase_types";
+import { AuthData } from "../data/auth_data";
+import UserModel from "../model/user_model";
+import AuthRepo from "./auth_repo";
+
+
+export default class AuthRepoImpl implements AuthRepo {
+    authData: AuthData;
+
+    constructor(authData: AuthData) {
+        this.authData = authData;
+    }
+
+    async signup<T, K>(param: T): Promise<K | null> {
+        try {
+            const res = await this.authData.signup(param as SignupParam);
+            return (res as UserModel | undefined) as K | null;
+        } catch (error) {
+            throw new Error(error as string);
+        }
+    }
+    async login<T, K>(param: T): Promise<K | null> {
+        try {
+            const res = await this.authData.login(param as LoginParam);
+            return (res as UserModel | undefined) as K | null;
+        } catch (error) {
+            throw new Error(error as string);
+        }
+    }
+
+}
