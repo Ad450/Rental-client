426c0af9e42dac066bb77b0603fd21fefe011b5b
Ad450 <adjeimanue@gmail.com>
2022-07-20 17:49:47 +0000

    minor changes

--- client/src/core/interfaces/usecase.ts
+++ client/src/core/interfaces/usecase.ts
@@ -1,6 +1,3 @@
-// export default interface UsecaseAlias<T, K> {
-//     method: (param: T): Promise<K | null> => {} ;
-// }
 
 export default abstract class UsecaseAlias<T, K>{
     abstract method(param: T): Promise<K | null>;
