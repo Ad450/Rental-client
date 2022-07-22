a095849b825412e6cabb9324ceb8c64b3b8a9489
Ad450 <adjeimanue@gmail.com>
2022-07-22 01:08:05 +0000

    common app styles

---
+++ client/src/core/components/styles/app_styles.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const RentalHeader = styled.div`
+background-image: url("https://images.unsplash.com/photo-1488707872600-5507977fe355?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8c2t5JTIwaG91c2V8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60");
+
+`
+
+export const RentalHeaderCard = styled.div`
+width: 90%
+`
+
+export const RentalFeatureProps = styled.div`
+padding: 30px 20px 20px 30px;
+`
+
+export const RentalWrapper = styled.div`
+
+`
