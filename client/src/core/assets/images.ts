551ca39910baedc442aaaeb4849dd48002d748ef
Ad450 <adjeimanue@gmail.com>
2022-07-23 16:26:40 +0000

    app images abstracted

---
+++ client/src/core/assets/images.ts
@@ -0,0 +1,20 @@
+export default abstract class Images {
+  /// [url1], [url2] and [url3] are the initial images rendered when the feature cards
+  /// render on screen
+  /// Can be used interchangeably as properties donot have a defining name .ie just randome images
+
+  static url1: string =
+    "https://watermark.lovepik.com/photo/20211203/large/lovepik-house-and-clear-sky-picture_501469477.jpg";
+  static url2: string =
+    "https://thumbs.dreamstime.com/b/grey-house-exterior-blue-sky-background-two-garage-spaces-northwest-usa-86342807.jpg";
+  static url3: string =
+    "https://images.unsplash.com/photo-1598603082415-fd487cb6471e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cmVkJTIwaG91c2V8ZW58MHx8MHx8&w=1000&q=80";
+
+  static imageUrlList: Array<string> = [
+    "https://thumbs.dreamstime.com/b/fragment-small-wooden-house-sky-background-32758690.jpg",
+    "https://thumbs.dreamstime.com/b/house-sky-14585841.jpg",
+    "https://image.shutterstock.com/image-photo/top-house-nice-window-260nw-508008430.jpg",
+    "https://thumbs.dreamstime.com/b/grey-house-exterior-blue-sky-background-two-garage-spaces-northwest-usa-86342807.jpg",
+    "https://thumbs.dreamstime.com/b/roof-house-against-clear-sky-7681354.jpg",
+  ];
+}
