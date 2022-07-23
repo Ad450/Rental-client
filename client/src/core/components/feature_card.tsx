46140cf159e822a0cfcfa2bfdee6a18dde407676
Ad450 <adjeimanue@gmail.com>
2022-07-23 16:27:27 +0000

    removed unneeded styles

--- client/src/core/components/feature_card.tsx
+++ client/src/core/components/feature_card.tsx
@@ -1,37 +1,12 @@
 import React from "react";
 import styled from "styled-components";
-import { StyledButton, StyledText } from "./styles/app_styles";
-import TextCard from "./styles/text_card";
+import {  StyledFeatureCard, StyledText } from "./styles/app_styles";
+import TextCard from "./text_card";
+import { useImage, useClearInterval } from "../hooks/useImage";
+import FeatureCardChild from "./feature_card_child";
 
-interface IStyledFeatureCard {
-  url: string;
-  height: string;
-  width: string;
-}
 
-interface IFeatureCardChild {
-  leadingText: string;
-  subText: string;
-  trailingAmount: string;
-  height: string;
-  width: string;
-  url: string;
-}
 
-const StyledFeatureCard = styled.div<IStyledFeatureCard>`
-  height: ${(props) => props.height};
-  width: ${(props) => props.width};
-  background-image: url(${(props) => props.url});
-  background-size: cover;
-  border-radius: 0.6rem;
-  text-align: start;
-  margin: 0px 15px 0px;
-
-  &:hover {
-    background-image: url(${(props) => props.url});
-    box-shadow: 10px 10px 10px #aaaaaa;
-  }
-`;
 
 const StyledRatingChild = styled.div`
   height: 150px;
@@ -66,30 +41,16 @@ const StyledRating = styled.div`
   align-items: baseline;
 `;
 
-const StyledFeatureCardChild = styled.div` 
-  display: flex;
-  flex-direction: column;
-  text:align: start;
-  padding-left:10px;
-`;
 
-const StyledTrailingCardChild = styled.div`
-  position: relative;
-  top: 220px;
-  display: flex;
-  flex-direction: row;
-  align-items: baseline;
-  justify-content: space-between;
-`;
 
 export default function FeatureCard() {
+  const [url1, url2, url3] = useImage();
+
+  useClearInterval();
+
   return (
     <div className="flex px-24 my-4 flex-wrap">
-      <StyledFeatureCard
-        height="250px"
-        width="270px"
-        url="https://watermark.lovepik.com/photo/20211203/large/lovepik-house-and-clear-sky-picture_501469477.jpg"
-      >
+      <StyledFeatureCard height="250px" width="270px" url={url1}>
         <StyledRatingChild>
           <StyledRatingImage
             src="https://cdn-icons-png.flaticon.com/512/695/695530.png"
@@ -115,7 +76,7 @@ export default function FeatureCard() {
         trailingAmount="$2,895,000"
         height="350px"
         width="270px"
-        url="https://images.unsplash.com/photo-1598603082415-fd487cb6471e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cmVkJTIwaG91c2V8ZW58MHx8MHx8&w=1000&q=80"
+        url={url2}
       ></FeatureCardChild>
       <FeatureCardChild
         leadingText="North Franklin Canyor"
@@ -123,47 +84,9 @@ export default function FeatureCard() {
         trailingAmount="$2,895,000"
         height="350px"
         width="270px"
-        url="https://res.cloudinary.com/twenty20/private_images/t_watermark-criss-cross-10/v1619175899000/photosp/cf53aef4-5806-435e-8b86-7c4c473a9ad7/stock-photo-building-house-weathered-clear-sky-clouds-scenery-blue-sky-singapore-singapore-city-cf53aef4-5806-435e-8b86-7c4c473a9ad7.jpg"
+        url={url3}
       ></FeatureCardChild>
       <TextCard></TextCard>
     </div>
-  );
-}
-
-function FeatureCardChild({
-  leadingText,
-  subText,
-  trailingAmount,
-  height,
-  url,
-  width,
-}: IFeatureCardChild) {
-  return (
-    <StyledFeatureCard height={height} width={width} url={url}>
-      <StyledFeatureCardChild>
-        <div>
-          <StyledText color="white" fontSize="25px" fontWeight="500">
-            {leadingText}
-          </StyledText>
-          <StyledText color="white" fontSize="18px" fontWeight="400">
-            {subText}
-          </StyledText>
-        </div>
-        <StyledTrailingCardChild>
-          <StyledText color="white" fontSize="20px">
-            {trailingAmount}
-          </StyledText>
-          <StyledButton
-            paddingX="30px"
-            paddingY="10px"
-            color="white"
-            backgroundColor="orange"
-          >
-            {" "}
-            →
-          </StyledButton>
-        </StyledTrailingCardChild>
-      </StyledFeatureCardChild>
-    </StyledFeatureCard>
   );
 }
